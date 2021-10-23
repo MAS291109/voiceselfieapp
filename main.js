27f9ddbcaee3787fe8dd6d0ed7c77bbcab43762f
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2021-10-23 12:41:35 +0530

    Add files via upload
    added camera and text to speech function.

--- main.js
+++ main.js
@@ -13,4 +13,21 @@ recognition.onresult = function run(event){
     console.log(content);
 
     document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    var speak_data = document.getElementById("textbox").value;
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width: 360,
+    height: 200,
+    image_format: 'png',
+    png_quality : 90
+});
+var camera = document.getElementById("camera");
